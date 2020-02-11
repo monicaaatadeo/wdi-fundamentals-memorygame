d672a293174959b99aceb0acce7bf1a83802da02
monicaaatadeo <monicaaatadeo@gmail.com>
2020-02-11 01:20:15 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -3,37 +3,32 @@ console.log("Up and running!");
 
 
 var cards = ["queen","queen","king","king"];
-var cardsInPlay = [ ];
+var cardsInPlay = [];
 
 
-
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-
-
-if (cardsInPlay.length === 2)
-
-	if (cardsInPlay[0] !== cardsInPlay[1])
-	{
-		alert ("You found a match!");
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log ("You found a match!");
+	} else {
+		console.log ("Sorry, try again.");
 	}
-	
-	
-	
+}
 		
 
-    else { 
-    	alert("Sorry, try again");
-    }
 
-		
-	
+var flipCard = function flipCard(cardID) {
 
-	 
+	console.log ("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+
+
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+	}
+}
+
+
+
+flipCard (0);
+flipCard (2);
+
